dbcdc03347350f81812508732d69dfdcecc3feaa
Anderson Lobo Feitosa <andersonlf@gmail.com>
2018-08-16 11:46:28 -0300

    make it better

--- poc-spring-build-properties-lib/src/main/java/com/app/VersionController.java
+++ poc-spring-build-properties-lib/src/main/java/com/app/VersionController.java
@@ -1,27 +1,31 @@
 package com.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersionController {
 
-  @Value("${spring.application.name}")
-  private String springAppName;
-
   @Autowired
-  private BuildProperties buildProperties;
+  private ApplicationContext applicationContext;
 
   @GetMapping("/version")
   public String version() {
+    String appName = "application-name-default";
+    String version = "1.0.0-default";
 
-    if (buildProperties != null) {
-      return String.format("%s %s", springAppName, buildProperties.getVersion());
+    if (applicationContext.containsBean("buildProperties")) {
+      BuildProperties buildProperties = applicationContext.getBean(BuildProperties.class);
+      version = buildProperties.getVersion();
     }
 
-    return String.format("%s %s", springAppName, "default");
+    if (!"application".equalsIgnoreCase(applicationContext.getId())) {
+      appName = applicationContext.getId();
+    }
+
+    return String.format("%s - %s", appName, version);
   }
 }
